0b98707a2ef310f9c84d7b4ff44b01aa286d2f56
Jacob Gonos <jacobgonos@gmail.com>
2022-08-30 21:57:31 -0400

    Started Styling
    Making this easier to look at while developing.

--- src/GameCanvas.js
+++ src/GameCanvas.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CSVLink} from 'react-csv';
 
 var xTiles = 64;
 var yTiles = 32;
@@ -6,7 +7,9 @@ var yTiles = 32;
 var tileHeight = Math.floor(window.innerHeight / yTiles);
 var tileWidth = Math.floor(window.innerWidth / xTiles);
 
-let tiles = [];
+var tiles = [];
+
+var allowEdit = false;
 
 class Tile {
     constructor(x, y, active) {
@@ -18,8 +21,9 @@ class Tile {
         if (this.active) {
             document.getElementById("gameCanvas").getContext('2d').beginPath();
             document.getElementById("gameCanvas").getContext('2d').rect(this.corner[0], this.corner[1], tileWidth, tileHeight);
+            document.getElementById("gameCanvas").getContext('2d').strokeStyle = '#3a5122';
             document.getElementById("gameCanvas").getContext('2d').stroke();
-            document.getElementById("gameCanvas").getContext('2d').fillStyle = "grey";
+            document.getElementById("gameCanvas").getContext('2d').fillStyle = '#5d782e';
             document.getElementById("gameCanvas").getContext('2d').fill();
         }
     }
@@ -35,21 +39,55 @@ for (let i = 0; i < xTiles; i++) {
     }
 }
 
+LoadTileLayout();
+
 function GameCanvas() {
+
     return (
-        <div className="App">
-            <canvas onClick={(e) => GetMouseOnCanvas(e)} id="gameCanvas" width={tileWidth * xTiles} height={tileHeight * yTiles} style={{ background: "#eee", width: "50%", height: "50%", padding: 0, margin: "auto", display: "block" }}></canvas>
-        </div>
+        <React.Fragment>
+            <canvas onClick={(e) => GetMouseOnCanvas(e)} id="gameCanvas" width={tileWidth * xTiles} height={tileHeight * yTiles} className="GameCanvas"></canvas>
+            <button type="button" onClick={() => ToggleEditable}>Edit</button>
+            <BoardExport />
+        </React.Fragment>
     );
 }
 
+class BoardExport extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentTiles: [],
+            loading: false
+        }
+    }
+
+    getTiles = () => {
+        this.setState({ loading: true });
+        this.state.currentTiles = tiles;
+        this.setState({ loading: false });
+    }
+
+    render() {
+        return (<CSVLink data={this.state.currentTiles} onClick={this.getTiles} asyncOnClick={true} hidden>Export Board</CSVLink>);
+    }
+}
+
 function GetMouseOnCanvas(e) {
+
+    if (!allowEdit)
+        return;
+
     var cRect = document.getElementById("gameCanvas").getBoundingClientRect();
     var canvasX = Math.round(e.clientX - cRect.left);
     var canvasY = Math.round(e.clientY - cRect.top);
     var tile = GetTile(canvasX * 2, canvasY * 2);
     tile.toggle();
-    drawboard();
+}
+
+function ToggleEditable() {
+    allowEdit = !allowEdit;
 }
 
 function GetTile(x, y) {
@@ -69,15 +107,118 @@ function GetTile(x, y) {
     return curTile;
 }
 
-function drawboard() {
+function ClearBoard() {
     document.getElementById("gameCanvas").getContext('2d').clearRect(0, 0, document.getElementById("gameCanvas").width, document.getElementById("gameCanvas").height);
+}
 
+function DrawBoard() {
     tiles.forEach(
         tile => {
             tile.drawTile();
         });
 
     document.getElementById("gameCanvas").getContext('2d').closePath();
+}
+
+function LoadTileLayout() {
+    const filePath = "http://localhost:3000/DefaultTileLayout.csv";
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onload = function () {
+
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var content = xmlhttp.responseText;
+            var headers = content.replace("\"corner\",\"active\"\n", '');
+            var rows = headers.split("\n");
+            let i = 0;
+
+            rows.forEach((row) => { 
+                var cleanRow = row.replaceAll('\"', '');
+                var data = cleanRow.split(',');
+                if (data[2] == "true")
+                    tiles[i].toggle();
+
+                i++;
+            });
+
+            DrawBoard();
+        }
+    }
+
+    xmlhttp.open("GET", filePath, true);
+
+    xmlhttp.send();
+}
+
+setInterval(UpdateGunner, 10);
+
+class Gunner{
+    constructor(x, y, bodyWidth, bodyHeight, gunWidth, gunHeight) {
+        this.pos = [x, y];
+        this.bodyWidth = bodyWidth;
+        this.bodyHeight = bodyHeight;
+        this.gunWidth = gunWidth;
+        this.gunHeight = gunHeight;
+        this.dest = null;
+    }
+
+    DrawGunner() {
+        //draw gun
+        document.getElementById("gameCanvas").getContext('2d').beginPath();
+        document.getElementById('gameCanvas').getContext('2d').rect((this.pos[0] + (this.bodyWidth / 2) - (this.gunWidth/2)), (this.pos[1] - (this.bodyHeight)), this.gunWidth, this.gunHeight);
+        document.getElementById("gameCanvas").getContext('2d').strokeStyle = '#3a5122';
+        document.getElementById('gameCanvas').getContext('2d').stroke();
+        document.getElementById('gameCanvas').getContext('2d').fillStyle = "#5d782e";
+        document.getElementById('gameCanvas').getContext('2d').fill();
+        document.getElementById('gameCanvas').getContext('2d').closePath();
+
+        //draw body
+        document.getElementById("gameCanvas").getContext('2d').beginPath();
+        document.getElementById('gameCanvas').getContext('2d').rect(this.pos[0], this.pos[1], this.bodyWidth, this.bodyHeight);
+        document.getElementById("gameCanvas").getContext('2d').strokeStyle = '#3a5122';
+        document.getElementById('gameCanvas').getContext('2d').stroke();
+        document.getElementById('gameCanvas').getContext('2d').fillStyle = "#5d782e";
+        document.getElementById('gameCanvas').getContext('2d').fill();
+        document.getElementById('gameCanvas').getContext('2d').closePath();
+    }
+
+    Move(dir) {
+
+        if (dir > 0 && (this.pos[0] + this.bodyWidth) < (tileWidth * xTiles) - tileWidth)
+            this.pos[0] += dir;
+
+        if (dir < 0 && this.pos[0] > tileWidth)
+            this.pos[0] += dir;
+    }
+
+    GoToDest() {
+
+        if (this.dest == null)
+            return;
+
+        if (Math.floor(this.pos[0] + (this.bodyWidth / 2)) == this.dest) {
+            this.dest = null;
+            return;
+        }
+
+        this.Move(this.pos[0] + (this.bodyWidth / 2) > this.dest ? -1 : 1);
+    }
+}
+
+const gunner = new Gunner(tileWidth * (xTiles / 2), tileHeight * (yTiles - 2), tileWidth * 1.3, (tileHeight / 2), (tileWidth/3), tileHeight);
+
+function UpdateGunner() {
+    ClearBoard();
+    DrawBoard();
+
+    if (gunner.dest == null) {
+        gunner.dest = Math.floor(Math.random() * 1200) + tileWidth;
+    }
+    else {
+        gunner.GoToDest();
+    }
+
+    gunner.DrawGunner();
 }
 
 export default GameCanvas;
