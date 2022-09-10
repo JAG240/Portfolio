0cfed804d1d7aa74e9d8885f2086fc2e49e7f533
Jacob Gonos <jacobgonos@gmail.com>
2022-09-10 15:59:33 -0400

    Correcting Links Instead
    Extension rename did not trigger a reupload of this. Changing in file instead now.

--- src/ProjectsContext.js
+++ src/ProjectsContext.js
@@ -8,49 +8,49 @@ export const ProjectsProvider = props => {
             id: 1,
             name: "Mock ERP",
             summary: "This project was created during my internship with eLoop.",
-            thumbnail: "erp_mock.png",
-            gif: "erp_mock.png"
+            thumbnail: "erp_mock.PNG",
+            gif: "erp_mock.PNG"
         },
         {
             id: 2,
             name: "Bird Game",
             summary: "My first game created without a set tutorial.",
-            thumbnail: "bird_game_thumb.png",
+            thumbnail: "bird_game_thumb.PNG",
             gif: "bird_game.gif"
         },
         {
             id: 3,
             name: "Pokemon Map",
             summary: "School project used to introduce me to basics of web development.",
-            thumbnail: "pokemon_map_thumb.png",
+            thumbnail: "pokemon_map_thumb.PNG",
             gif: "pokemon_map.gif"
         },
         {
             id: 4,
             name: "Hotdog Game",
             summary: "2D physics driven game designed as mobile app.",
-            thumbnail: "hotdog_game_thumb.png",
+            thumbnail: "hotdog_game_thumb.PNG",
             gif: "hotdog_game.gif"
         },
         {
             id: 5,
             name: "Machine Learning",
             summary: "Integrated library with unity to run re-enforcement learning algorithims.",
-            thumbnail: "machine_learning_thumb.png",
+            thumbnail: "machine_learning_thumb.PNG",
             gif: "machine_learning.gif"
         },
         {
             id: 6,
             name: "Colony Game",
             summary: "Inspired by Rimworld attempted to create a faithful clone and learned difficult topics.",
-            thumbnail: "colony_game_thumb.png",
+            thumbnail: "colony_game_thumb.PNG",
             gif: "colony_game.gif"
         },
         {
             id: 7,
             name: "Design Patterns",
             summary: "After reading design pattern book, I wanted to implement into project to apply the knowledge.",
-            thumbnail: "design_patterns_thumbnail.png",
+            thumbnail: "design_patterns_thumbnail.PNG",
             gif: "design_patterns.gif"
         },
         {
