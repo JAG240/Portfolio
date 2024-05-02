01ff275fe56e45999ffe027fe83ab3fa9ad40a29
Jacob Gonos <jacobgonos@gmail.com>
2024-05-02 07:41:19 -0400

    Responsive Design Started
    Started adding responsive design styling to allow for mobile users to have a good experience. Also, tried another pdf display solution.

--- src/modules/resume/Resume.js
+++ src/modules/resume/Resume.js
@@ -3,9 +3,9 @@ import './Resume.css';
 
 function Resume() {
     return (
-        <div className="resume-container">
-            <iframe className="resume-embed" src="Jacob_Gonos_Resume.pdf" />
-        </div>
+        <object data="Jacob_Gonos_Resume.pdf" type="application/pdf" className="resume-container" width="100%" height="100%">
+            <embed src="https://drive.google.com/file/d/11puuvS-Tf0xBZ8My8EhYm0ISpS9MPDGf/preview?usp=sharing" width="100%" height="100%"/>
+        </object>
     );
 }
 
