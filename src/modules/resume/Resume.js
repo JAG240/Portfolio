971ce6816f31017d22077bfe9a52ab441a9ff071
Jacob Gonos <jacobgonos@gmail.com>
2024-05-21 10:23:25 -0400

    Updated Resume
    After getting some feedback, I updated my resume to be more concise. Updating resume on website as well.

--- src/modules/resume/Resume.js
+++ src/modules/resume/Resume.js
@@ -6,7 +6,7 @@ function Resume() {
     return (
         <div className="resume">
             <object data="Jacob_Gonos_Resume.pdf" type="application/pdf" className="resume-container" width="100%" height="100%">
-                <embed src="https://drive.google.com/file/d/11puuvS-Tf0xBZ8My8EhYm0ISpS9MPDGf/preview?usp=sharing" width="100%" height="100%"/>
+                <embed src="https://drive.google.com/file/d/16SdEzeqITtl6AZuXIjo8P9opfMlajLK2/preview?usp=sharing" width="100%" height="100%"/>
             </object>
 
             <div className="link-boxes">
